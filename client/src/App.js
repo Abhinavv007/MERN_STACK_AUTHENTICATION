0a1a7c68209a4bf72c7cee7972c6b32b61aab5f3
Abhinavv007 <119091168+Abhinavv007@users.noreply.github.com>
2023-10-12 23:13:01 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,16 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import ProtectedRoutes from "./components/ProtectedRoutes"
+import ForgetPassword from "./components/ForgetPassword";
+import SaveForgetPass from "./components/SaveForgetPass";
 
 function App() {
   return (
     <div className="App">
      <Router>
       <Routes>
+      <Route path="/reset-password" element={<ForgetPassword />} />
+      <Route path="/user/reset/:id" element={<SaveForgetPass />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<ProtectedRoutes />}>
