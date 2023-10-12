1d5e0f9e81233fd7ab3556bbe84001cb83732c9a
Abhinavv007 <119091168+Abhinavv007@users.noreply.github.com>
2023-10-12 23:12:12 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,5 +5,9 @@ const router  = express.Router()
 
 router.post("/users/register",authController.userRegistration)
 router.post("/users/login",authController.userLogin)
+router.post("/forget-password",authController.forgetPassword)
+router.post("/reset-password",authController.forgetPassword)
+router.post("/reset-password/:id/",authController.saveForgetPassword)
+
 router.post("/change-password/:id",verifyToken,authController.changePassword)
 export default router
